f8bf84184a31cfab28eeb8e4be14f278c341af55
hamzaa101 <hamzahhudzaifah16@gmail.com>
2026-05-06 23:07:37 +0700

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import js from '@eslint/js';
+import globals from "globals";
+import daStyle from 'eslint-config-dicodingacademy';
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  daStyle,
+  { files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { globals: globals.browser } },
+]);

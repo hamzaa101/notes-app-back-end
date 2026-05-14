a193cd1e932972b33214cb32fcf7e60a994d7357
hamzaa101 <hamzahhudzaifah16@gmail.com>
2026-05-14 21:35:17 +0700

    adding joi for error-handling

--- src/services/notes/validator/schema.js
+++ src/services/notes/validator/schema.js
@@ -4,3 +4,7 @@ export const notePayloadSchema = Joi.object({
   body: Joi.string().required(),
   tags: Joi.array().items(Joi.string()).required(),
 });
+
+export const noteQuerySchema = Joi.object({
+  title: Joi.string().empty(),
+});

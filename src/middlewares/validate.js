2d26fcac6e3652fb36b6013d6d542d4d6f74067f
hamzaa101 <hamzahhudzaifah16@gmail.com>
2026-05-14 21:33:23 +0700

    update req.query

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,5 +1,5 @@
 const validate = (schema) => (req, res, next) => {
-  const { error, value } = schema.validate(req.body, {
+  const { error, value } = schema.validate(req.query, {
     abortEarly: false,
     allowUnknown: false,
     stripUnknown: true
@@ -9,5 +9,6 @@ const validate = (schema) => (req, res, next) => {
   req.validated = value;
   next();
 };
+
 
 export default validate;

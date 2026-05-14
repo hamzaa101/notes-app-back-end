ba1e17854ce799410ee6e10557d959c88851e965
hamzaa101 <hamzahhudzaifah16@gmail.com>
2026-05-14 21:34:25 +0700

    update method in getNotes

--- src/services/notes/controllers/note-controller.js
+++ src/services/notes/controllers/note-controller.js
@@ -22,6 +22,11 @@ export const createNote = (req, res, next) => {
 };
 
 export const getNotes = (req, res) => {
+  const { title = '' } = req.query;
+  if (title !== '') {
+    const note = notes.filter((note) => note.title === title);
+    return response(res, 200, 'success', { notes: note });
+  }
   return response(res, 200, 'success', { notes: notes });
 };
 

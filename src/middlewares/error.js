3153ea610ce29525bcc012b4759212307479c2e9
hamzaa101 <hamzahhudzaifah16@gmail.com>
2026-05-14 21:13:39 +0700

    adding joi validation error

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -7,6 +7,11 @@ const ErrorHandler = (err, req, res, next) => {
     return response(res, err.statusCode, err.message, null);
   }
 
+  // Handle Joi validation errors
+  if (err.isJoi) {
+    return response(res, 400, err.details[0].message, null);
+  }
+
   const status = err.statusCode || err.status || 500;
   const message = err.message || 'Internal Server Error';
 
